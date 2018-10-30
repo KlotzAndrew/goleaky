e04588dbf08833694e276af38f9488568bdb697b
Andrew Klotz <agc.klotz@gmail.com>
2018-10-30 03:14:21 +0000

    crash if OOM

--- repo/repo.go
+++ repo/repo.go
@@ -1,7 +1,16 @@
 package repo
 
+import (
+	"time"
+)
+
 var savedReports = []string{}
 
 func SaveReport(report string) {
 	savedReports = append(savedReports, report)
+
+	time.Sleep(10 * time.Microsecond)
+	if len(savedReports) > 1000000 {
+		panic("OOM")
+	}
 }
